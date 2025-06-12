d88b4b2a6333c6ccd4e5fd6f0babcd644134a7af
d-wojtowicz <dwojtowicz1@o2.pl>
2025-06-13 00:02:39 +0200

    update: server middlewares

--- backend-rest/server.js
+++ backend-rest/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 
 import cors from 'cors';
-import { json } from 'express';
 
 import CONFIG from './config/config.js';
 import log from './config/logging.js';
@@ -10,9 +9,26 @@ const NAMESPACE = CONFIG.server.env == 'PROD' ? 'SERVER' : 'server.js';
 
 const app = express();
 
-app.use(cors());
-app.use(json());
+app.use(
+  cors({
+    origin: '*', // TODO: Create and use a whitelist of allowed origins (if JWT with credentials is used/if environment is not DEV)
+    methods: ['GET','POST','PUT','DELETE','PATCH'],
+    allowedHeaders: ['Content-Type','Authorization']
+  })
+);  
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  log.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+  res.on('finish', () => {
+    log.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+  });
+
+  next();
+});
 
 app.listen(CONFIG.server.port, CONFIG.server.host, () => {
-    log.info(NAMESPACE, `Server is running at http://${CONFIG.server.host}:${CONFIG.server.port}`);
+  log.info(NAMESPACE, `Server is running at http://${CONFIG.server.host}:${CONFIG.server.port}`);
 });
