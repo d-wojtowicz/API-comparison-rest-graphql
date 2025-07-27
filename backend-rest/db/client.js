ecd879ffdd0d70cd33658c8c5cf25fced4c646df
d-wojtowicz <dwojtowicz1@o2.pl>
2025-07-27 22:02:53 +0200

    add: prisma logs

--- backend-rest/db/client.js
+++ backend-rest/db/client.js
@@ -5,6 +5,31 @@ import log from '../config/logging.js';
 const NAMESPACE = CONFIG.server.env == 'PROD' ? 'PRISMA' : 'db/client.js';
 const prisma = new PrismaClient({
   datasources: { db: { url: CONFIG.dbUrl } },
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+});
+
+// Log SQL queries
+prisma.$on('query', (e) => {
+  log.info(NAMESPACE, `Query: ${e.query}`);
+  log.info(NAMESPACE, `Params: ${e.params}`);
+  log.info(NAMESPACE, `Duration: ${e.duration}ms`);
 });
 
 prisma
